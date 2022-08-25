d69e70b1583185e80f8928e684bc1cb464a21c85
Gab0410 <gabrielmcorlando@gmail.com>
2022-08-25 18:50:49 -0300

    Adding set/get and Constructor

--- src/students/student.entity.ts
+++ src/students/student.entity.ts
@@ -2,9 +2,45 @@ import { Department } from '../departments/department.entity';
 import { User } from '../users/user.entity';
 
 export class Student extends User {
-    ra!: number;
+    private ra!: number;
 
-    department!: Department;
+    private department!: Department;
 
-    library_pendencies!: boolean;
+    private library_pendencies!: boolean;
+
+    public getRA(): number{
+        return this.ra;
+    }
+
+    public getDepartment(): Department{
+        return this.department;
+    }
+
+    public getLibraryPendencies(): boolean{
+        //Aqui será necessário integrar com o outro sistema, o de biblioteca
+        return this.library_pendencies
+    }
+
+    public setRA(ra:number): void{
+        //Fazer a verificação?
+        this.ra = ra;
+    }
+
+    public setDepartment(dep: Department): void{
+        this.department = dep;
+    }
+
+    public setLibraryPendencies(library_pendencies: boolean): void{
+        this.library_pendencies = library_pendencies;
+    }
+
+    constructor(id:number, name:string, cpf:string, email:string, birth_date: Date, password: string, ra:number,dep:Department,library_pendencies: boolean){
+
+        super(id, name, cpf, email, birth_date, password); //Perguntar pro taviola por que precisa disso
+        this.setRA(ra);
+        this.setDepartment(dep);
+        this.setLibraryPendencies(library_pendencies);
+    }
+    
 }
+
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,13 +1,73 @@
 export class User {
-    id!: number;
+    private id!: number;
 
-    name!: string;
+    private name!: string;
 
-    cpf!: string;
+    private cpf!: string;
 
-    email!: string;
+    private email!: string;
 
-    birth_date!: Date;
+    private birth_date!: Date;
 
-    password!: string;
+    private password!: string;
+
+    public getId():number{
+        return this.id;
+    }
+
+    public getName():string{
+        return this.name;
+    }
+
+    public getCPF():string{
+        return this.cpf;
+    }
+
+    public getEmail(): string{
+        return this.email;
+    }
+
+    public getBirthDate():Date{
+        return this.birth_date;
+    }
+
+    //Deve ter um método público?
+    public getPassword(): string{
+        return this.password;
+    }
+
+    public setId(id:number):void{
+        this.id = id;
+    }
+
+    public setName(name: string):void{
+        this.name = name;
+    }
+
+    public setCPF(cpf:string): void{
+        this.cpf = cpf;
+    }
+
+    public setEmail(email:string):void{
+        this.email = email;
+    }
+
+    public setBirthDate(birth_date:Date):void{
+        this.birth_date = birth_date;
+    }
+
+    public setPassword(password:string): void{
+        this.password = password;
+    }
+
+    constructor(id:number, name:string, cpf:string, email:string, birth_date: Date, password: string){
+
+        this.setId(id);
+        this.setName(name);
+        this.setCPF(cpf);
+        this.setEmail(email);
+        this.setBirthDate(birth_date);
+        this.setPassword(password);
+    }
+
 }
